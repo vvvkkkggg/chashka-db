359f691f3f69a549a928c0dc4b2d836f2c72aa6b
Alexey Kosenko <ayukosenko@edu.hse.ru>
2023-12-10 12:29:36 +0300

    refactor(schema): update schema naming, update phys model

--- schema.sql
+++ schema.sql
@@ -1,20 +1,7 @@
-DROP SCHEMA IF EXISTS chashka;
-CREATE SCHEMA chashka;
+CREATE SCHEMA IF NOT EXISTS chashka;
 
-DROP TABLE IF EXISTS chashka.coffeeshop;
-DROP TABLE IF EXISTS chashka.barista;
-DROP TABLE IF EXISTS chashka.owner;
-DROP TABLE IF EXISTS chashka.item;
-DROP TABLE IF EXISTS chashka."user";
-DROP TABLE IF EXISTS chashka.receipt;
-DROP TABLE IF EXISTS chashka.reward;
-DROP TABLE IF EXISTS chashka.user_reward;
-DROP TABLE IF EXISTS chashka.user_coffeeshop;
-DROP TABLE IF EXISTS chashka.owner_coffeeshop;
-DROP TABLE IF EXISTS chashka.item_receipt;
-
-CREATE TABLE chashka.coffeeshop (
-    shop_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS chashka.coffee_shop (
+    coffee_shop_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     inn VARCHAR(10) NOT NULL,
     kkt VARCHAR(16) UNIQUE NOT NULL,
@@ -22,74 +9,79 @@ CREATE TABLE chashka.coffeeshop (
     address TEXT NOT NULL
 );
 
-CREATE TABLE chashka.barista (
-    worker_id SERIAL PRIMARY KEY,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE SET NULL,
+CREATE TABLE IF NOT EXISTS chashka.barista (
+    barista_id SERIAL PRIMARY KEY,
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE SET NULL,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE chashka.owner (
+CREATE TABLE IF NOT EXISTS chashka.owner (
     owner_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
 );
 
-CREATE TABLE chashka.item (
-    product_id SERIAL PRIMARY KEY,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS chashka.item (
+    item_id SERIAL PRIMARY KEY,
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL,
     CHECK (price >= 0)
 );
 
-CREATE TABLE chashka."user" (
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS chashka.customer (
+    customer_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE chashka.receipt (
+CREATE TABLE IF NOT EXISTS chashka.receipt (
     receipt_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES chashka."user"(user_id) ON DELETE SET NULL,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE SET NULL,
+    customer_id INTEGER REFERENCES chashka.customer(customer_id) ON DELETE SET NULL,
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE SET NULL,
     total INTEGER NOT NULL,
     qr_data TEXT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
+    created TIMESTAMP NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CHECK (total >= 0)
 );
 
-CREATE TABLE chashka.reward (
+CREATE TABLE IF NOT EXISTS chashka.reward (
     reward_id SERIAL PRIMARY KEY,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE CASCADE,
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL,
     image_url VARCHAR(255),
     CHECK (price >= 0)
 );
 
-CREATE TABLE chashka.user_reward (
-    user_id INTEGER REFERENCES chashka."user"(user_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS chashka.customer_reward (
+    customer_id INTEGER REFERENCES chashka.customer(customer_id) ON DELETE CASCADE,
     reward_id INTEGER REFERENCES chashka.reward(reward_id) ON DELETE CASCADE,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, reward_id)
+    PRIMARY KEY (customer_id, reward_id)
 );
 
-CREATE TABLE chashka.user_coffeeshop (
-    user_id INTEGER REFERENCES chashka."user"(user_id) ON DELETE CASCADE,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS chashka.coffee_shop_customer (
+    customer_id INTEGER REFERENCES chashka.customer(customer_id) ON DELETE CASCADE,
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE CASCADE,
     bonuses INTEGER DEFAULT 0,
-    PRIMARY KEY (user_id, shop_id),
+    PRIMARY KEY (customer_id, coffee_shop_id),
     CHECK (bonuses >= 0)
 );
 
-CREATE TABLE chashka.owner_coffeeshop (
+CREATE TABLE IF NOT EXISTS chashka.coffee_shop_owner (
     owner_id INTEGER REFERENCES chashka.owner(owner_id) ON DELETE CASCADE,
-    shop_id INTEGER REFERENCES chashka.coffeeshop(shop_id) ON DELETE CASCADE,
-    PRIMARY KEY (owner_id, shop_id)
+    coffee_shop_id INTEGER REFERENCES chashka.coffee_shop(coffee_shop_id) ON DELETE CASCADE,
+    PRIMARY KEY (owner_id, coffee_shop_id)
 );
 
-CREATE TABLE chashka.item_receipt (
-    product_id INTEGER REFERENCES chashka.item(product_id) ON DELETE SET NULL,
-    receipt_id INTEGER REFERENCES chashka.receipt(receipt_id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS chashka.receipt_item (
+    item_id INTEGER REFERENCES chashka.item(item_id) ON DELETE SET NULL,
+    receipt_id INTEGER REFERENCES chashka.receipt(receipt_id) ON DELETE CASCADE,
+    amount INTEGER NOT NULL,
+    PRIMARY KEY (item_id, receipt_id),
+    CHECK (amount > 0)
 );
