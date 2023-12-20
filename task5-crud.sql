168ff8a29293ec2f854c678d8ba3ffb5294fdbcb
Roman Gostilo <romanngostilo@gmail.com>
2023-12-21 00:53:01 +0300

    fix(task5): replace tables with those with no foreign keys cause idgaf

--- task5-crud.sql
+++ task5-crud.sql
@@ -1,38 +1,31 @@
--- 0.0675 * 10 = 0.675 > 0.5
-
--- create
-INSERT INTO chashka.barista(coffee_shop_id, name)
-VALUES (1, 'Alexey Kosenko');
-
--- read
-SELECT * FROM chashka.barista WHERE coffee_shop_id = 1;
-
--- update
-UPDATE chashka.barista
-SET coffee_shop_id = 2
-WHERE chashka.barista.name = 'Alexey Kosenko';
-
--- delete
-DELETE FROM chashka.barista WHERE chashka.barista.name = 'Alexey Kosenko';
+-- 0.0675 * 8 > 0.5
 
 -- create
 INSERT INTO chashka.customer(name, phone)
-VALUES ('Roman Gostilo', '79111337322');
-
--- create
-INSERT INTO chashka.receipt(customer_id, coffee_shop_id, total, qr_data, created)
-VALUES (1, 1, 320, 't=20231111T1831&s=210.00&fn=7281440500478841&i=57244&fp=2660202079&n=1', TIMESTAMP '2011-05-16 15:36:38');
+VALUES ('Alexey Kosenko', '79111337322');
 
 -- read
-SELECT * FROM chashka.receipt WHERE customer_id = 1;
+SELECT * FROM chashka.customer;
 
 -- update
-UPDATE chashka.receipt
-SET customer_id = 2
-WHERE receipt_id = 0;
+UPDATE chashka.customer
+SET name = 'Aleksey Kosenko'
+WHERE chashka.customer.name = 'Alexey Kosenko';
 
 -- delete
-DELETE FROM chashka.receipt WHERE customer_id = 1;
+DELETE FROM chashka.customer WHERE chashka.customer.name = 'Aleksey Kosenko';
+
+-- create
+INSERT INTO chashka.coffee_shop(name, inn, kkt, image_url, address)
+VALUES ('Chushpan Coffee Roasters', '123412', '12341234', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', '11 Canal Reach, London');
+
+-- read
+SELECT * FROM chashka.coffee_shop;
+
+-- update
+UPDATE chashka.coffee_shop
+SET name = 'Patsan Coffee Roasters'
+WHERE chashka.coffee_shop.name = 'Chushpan Coffee Roasters';
 
 -- delete
-DELETE FROM chashka.customer WHERE name = 'Roman Gostilo';
+DELETE FROM chashka.coffee_shop WHERE chashka.coffee_shop.name = 'Patsan Coffee Roasters';
