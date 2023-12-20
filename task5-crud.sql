1693ffdeda2c4d3421cbde0d9d96a4172ae634e9
Roman Gostilo <romanngostilo@gmail.com>
2023-12-21 00:33:30 +0300

    fix(task5): add customer insertion and deletion for constraints consistency

--- task5-crud.sql
+++ task5-crud.sql
@@ -1,4 +1,4 @@
--- 0.0675 * 8 = 0.54 > 0.5
+-- 0.0675 * 10 = 0.675 > 0.5
 
 -- create
 INSERT INTO chashka.barista(coffee_shop_id, name)
@@ -16,6 +16,10 @@ WHERE chashka.barista.barista_id = 0;
 DELETE FROM chashka.barista WHERE chashka.barista.barista_id = 1;
 
 -- create
+INSERT INTO chashka.customer(name, phone)
+VALUES ('Roman Gostilo', '79111337322');
+
+-- create
 INSERT INTO chashka.receipt(customer_id, coffee_shop_id, total, qr_data, created)
 VALUES (1, 1, 320, 't=20231111T1831&s=210.00&fn=7281440500478841&i=57244&fp=2660202079&n=1', TIMESTAMP '2011-05-16 15:36:38');
 
@@ -29,3 +33,6 @@ WHERE receipt_id = 0;
 
 -- delete
 DELETE FROM chashka.receipt WHERE customer_id = 1;
+
+-- delete
+DELETE FROM chashka.customer WHERE name = 'Roman Gostilo';
